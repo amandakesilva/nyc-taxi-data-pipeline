d3f7ac3cdf4500645fe7c60e2fbdb0ba5ac03d54
Amanda Silva <amandakesilva@gmail.com>
2026-05-17 18:00:21 -0300

    Finish docker setup

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,6 +7,7 @@ month = int(sys.argv[1])
 print(f"Running pipeline for month {month}")
 
 df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df['month'] = month
 print(df.head())
 
 df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
